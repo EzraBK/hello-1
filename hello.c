50bb049ac1a25089760148ccc9a21c39b61c3531
Zachary James Scott <zjscott@cse.unl.edu>
2018-01-25 15:34:21 -0600

    Initial Comit

---
+++ hello.c
@@ -0,0 +1,12 @@
+/**
+ * Author: Zac Scott
+ * Date: 2018/01/25
+ * This program will print the names of its authors when ran. 
+*/ 
+
+#include <stdio.h> 
+ 
+int main(int argc, char **argv) {
+  printf("Zac Scott\n");
+  return 0;
+}
